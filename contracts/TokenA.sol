5be91fc076ca64492163fd8d92f94ae46f09927f
darksuei <chrisraphael576@gmail.com>
2023-10-08 16:55:14 +0100

    (feat):import from remix

---
+++ contracts/TokenA.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
+contract TokenA is ERC20, Ownable, ERC20Permit {
+    constructor() ERC20("TokenA", "TOA") ERC20Permit("TokenA") {
+        _mint(msg.sender, 1000000 * 10 ** decimals());
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
