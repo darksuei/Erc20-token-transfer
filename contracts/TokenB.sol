64641c5e57d2587360245aba1eafdf51695248d4
darksuei <chrisraphael576@gmail.com>
2023-10-09 12:13:06 +0100

    Use compiler 0.8.19

--- contracts/TokenB.sol
+++ contracts/TokenB.sol
@@ -1,13 +1,18 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.19;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 
 contract TokenB is ERC20, Ownable, ERC20Permit {
-    constructor() ERC20("TokenB", "TOB") ERC20Permit("TokenB") {
+    constructor(address initialOwner)
+        ERC20("TokenB", "TOB")
+        ERC20Permit("TokenB")
+        Ownable(initialOwner)
+    {
         _mint(msg.sender, 1000000 * 10 ** decimals());
+
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
